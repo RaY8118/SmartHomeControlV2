a77a4718efa3f78330ff2fda6d7f73f2d53bf1f4
RaY8118 <parthghadi43@gmail.com>
2024-09-23 20:05:20 +0530

    convert website to PWA with service worker and manifest setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration.js';  // <-- Import service worker
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
+
+// Register the service worker to enable PWA functionality
+serviceWorkerRegistration.register();  // <-- Register service worker here
